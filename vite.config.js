8694a65e20520e4794e5842b4683aaf27cb86cfe
webcro1 <webcro1@outlook.com>
2025-07-11 13:43:53 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',            // URLs en relatif
+  server: {
+
+    host: true, // Accept connections from the local network
+    port: 5173, // You can change this if needed
+
+  },
+  build: {
+    assetsDir: 'assets'  // place fonts/css/js/images sous dist/assets
+  },
   plugins: [
     vue(),
     vueDevTools(),
